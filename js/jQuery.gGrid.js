35691fefe8fa9ca69a11ce2679455dbaa9c83603
shiftie <g.dzinovic@gmail.com>
2012-11-18 20:29:16 +0100

    debug loop related performance

--- js/jQuery.gGrid.js
+++ js/jQuery.gGrid.js
@@ -33,7 +33,7 @@ Ggrid = {
 	 		for (i = 0; i < self.cellPerRow; i++) {
 	 			offsetW = i * self.cellSize;
 	 			offsetH = (self.currentRow - 1) * self.cellSize;
-	 			cell = $('<div></div>').attr('class', 'cell').attr('id', 'grid'+gridIndex+'cell'+self.currentRow+'-'+i)
+	 			cell = $('<div></div>').attr('class', 'cell hidden').attr('id', 'grid'+gridIndex+'cell'+self.currentRow+'-'+i)
 	 								   .width(self.cellSize).height(self.cellSize)
 	 								   .css('left', offsetW).css('top', offsetH)
 	 								   .css('opacity', 0);
@@ -45,7 +45,7 @@ Ggrid = {
 	 	for (i = 0; i < self.cellPerRow; i++) {
  			offsetW = i * self.cellSize;
  			offsetH = (self.currentRow - 1) * self.cellSize;
- 			cell = $('<div></div>').attr('class', 'cell').attr('id', 'grid'+gridIndex+'cell'+self.currentRow+'-'+i)
+ 			cell = $('<div></div>').attr('class', 'cell hidden').attr('id', 'grid'+gridIndex+'cell'+self.currentRow+'-'+i)
  								   .width(self.cellSize).height(self.cellSize)
  								   .css('left', offsetW).css('top', offsetH)
  								   .css('opacity', 0);
@@ -74,43 +74,58 @@ function randomXToY(minVal,maxVal,floatVal)
 	};  
     
   	var options = $.extend(defaults, options);  
-  
-    return this.each(function() {  
+  	
+  	var timer;
+
+    $(window).scroll(function() {
+    	clearTimeout(timer);
+        timer = setTimeout( refresh , 150 );
+	});
+
+	function refresh() {
+		console.log('in2');
+		visibleAreaTop = $(window).scrollTop();
+		visibleAreaBottom = $(window).scrollTop() + $(window).height();
+
+		//for each grid
+		for(g = 0; g < gGrids.length; g++){
+			currentGrid = gGrids[g];
+			if(currentGrid.isFadedIn == false){
+				for (i = 1; i <= currentGrid.totalRaws; i++) {
+	 			//if row is displayed in window
+	 			if((visibleAreaBottom >  $('#grid'+g+'cell'+i+'-0').offset().top) && (visibleAreaTop < $('#grid'+g+'cell'+i+'-0').offset().top)){
+	 				//if not already visible
+	 				if($('#grid'+g+'cell'+i+'-0').hasClass('hidden')){
+	 					for (j = 0; j < currentGrid.cellPerRow; j++) {
+	 						$('#grid'+g+'cell'+i+'-'+j).removeClass('hidden');
+	 						delay = (randomXToY(options.minDelay, options.maxDelay, 2))+'s';	 						
+	 						delayString = '-moz-transition-delay:'+delay+'; -webkit-transition-delay:'+delay+'; -o-transition-delay:'+delay+'; transition-delay:'+delay+';';
+	 						//console.log($('#grid'+g+'cell'+i+'-'+j).attr('style'));
+	 						$('#grid'+g+'cell'+i+'-'+j).attr('style', $('#grid'+g+'cell'+i+'-'+j).attr('style') + delayString);
+	 						/*$('#grid'+g+'cell'+i+'-'+j).css('-moz-transition-delay', delay)
+	 										  .css('-webkit-transition-delay', delay)
+	 										  .css('-o-transition-delay', delay)
+	 										  .css('transition-delay', delay);*/
+							$('#grid'+g+'cell'+i+'-'+j).css('opacity', 1);
+						};
+	 				}				
+				}
+	 		};
+			}
+		}
+	}
+
+	$(window).scroll();
+
+	 return this.each(function() {  
     	obj = $(this); 
   		
   		gGrid = Ggrid;						
 		gGrid.init(obj, gGrids.length, options);								
 		gGrids.push($.extend({}, gGrid));
 
-		$(window).scroll(function() {
-			visibleAreaTop = $(window).scrollTop();
-			visibleAreaBottom = $(window).scrollTop() + $(window).height();
-
-			//for each grid
-			for(g = 0; g < gGrids.length; g++){
-				currentGrid = gGrids[g];
-				if(currentGrid.isFadedIn == false){
-					for (i = 1; i <= currentGrid.totalRaws; i++) {
-		 			//if row is displayed in window
-		 			if((visibleAreaBottom >  $('#grid'+g+'cell'+i+'-0').offset().top) && (visibleAreaTop < $('#grid'+g+'cell'+i+'-0').offset().top)){
-		 				//if not already visible
-		 				if($('#grid'+g+'cell'+i+'-0').css('opacity') != 1){
-		 					for (j = 0; j < currentGrid.cellPerRow; j++) {
-		 						delay = (randomXToY(options.minDelay, options.maxDelay, 2))+'s';
-		 						$('#grid'+g+'cell'+i+'-'+j).css('-moz-transition-delay', delay)
-		 										  .css('-webkit-transition-delay', delay)
-		 										  .css('-o-transition-delay', delay)
-		 										  .css('transition-delay', delay);
-								$('#grid'+g+'cell'+i+'-'+j).css('opacity', 1);
-							};
-		 				}				
-					}
-		 		};
-				}
-			}
-		});
-
-		$(window).scroll();
+		console.log('in');
 	});  
+
 };  
 })(jQuery); 
